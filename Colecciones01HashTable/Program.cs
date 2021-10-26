b4b0bce9a21d35dd3d348a2993aee03f5a2b0a18
jesusito02 <jesus_restar@hotmail.com>
2021-10-26 17:00:39 -0400

    26OCT2021

---
+++ Colecciones01HashTable/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+namespace Colecciones01HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable miTabla = new Hashtable();
+
+            //Adicionamos elementos
+            miTabla.Add(132, "Osado");
+            miTabla.Add(145, "Hello");
+            miTabla.Add(23, "Playa");
+            miTabla.Add(567, "Programacion");
+            miTabla.Add(12456, "C#");
+
+            //Mostramos los elementos
+            foreach (DictionaryEntry elemento in miTabla)
+                     Console.WriteLine("({0}, {1})", elemento.Key, elemento.Value);
+
+            //Si intentamos colocar un elemento con llave repetida, provocará una excepcion
+            //miTabla.Add(567, "Hola que tal");
+
+            Console.WriteLine("-----");
+
+            //Cantidad de elementos
+            Console.WriteLine(miTabla.Count);
+
+            Console.WriteLine("-----");
+
+            //Obtenemos el elemento de determinada llave
+            Console.WriteLine(miTabla[132]);
+            Console.WriteLine(miTabla[23]);
+
+            Console.WriteLine("-----");
+
+            //Colocamos un elemento en una llave
+            miTabla[12456] = "C-Sharp";
+
+            foreach (DictionaryEntry elemento in miTabla)
+                     Console.WriteLine("({0}, {1})", elemento.Key, elemento.Value);
+
+            Console.WriteLine("-----");
+
+            //Verificamos si hay un elemento
+            Console.WriteLine(miTabla.Contains(78));
+
+            //Eliminamos un elemento
+            miTabla.Remove(145);
+
+            foreach (DictionaryEntry elemento in miTabla)
+                     Console.WriteLine("({0}, {1})", elemento.Key, elemento.Value);
+
+            Console.WriteLine("-----");
+
+            //Podemos recorrer las llaves
+            foreach (int llave in miTabla.Keys)
+                     Console.WriteLine(llave);
+
+            Console.WriteLine("-----");
+
+            //Podemos recorrer los valores
+            foreach (int llave in miTabla.Keys)
+                     Console.WriteLine(llave);
+
+            Console.WriteLine("-----");
+
+        }
+    }
+}
---
+++ Colecciones01Queue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace Colecciones01Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue cola = new Queue(); //FIFO: First In, First Out
+
+            cola.Enqueue("Manzana");
+            cola.Enqueue("Fresa");
+            cola.Enqueue("Pera");
+            cola.Enqueue("Piña");
+            cola.Enqueue("Limón");
+
+            //Iteramos
+            foreach(var fruta in cola)
+                    Console.WriteLine($" {fruta}");
+
+            Console.WriteLine("-----");
+
+            //Obtenemos el primer objeto insertado
+            Console.WriteLine(cola.Peek());
+
+            Console.WriteLine("-----");
+
+            //Extraemos elementos de la Cola
+            Console.WriteLine("Dequeue {0}", cola.Dequeue());
+            Console.WriteLine("Dequeue {0}", cola.Dequeue());
+
+            //Iteramos los elementos que quedan en la Cola
+            foreach(var fruta in cola)
+                    Console.WriteLine($"{fruta}");
+
+            Console.WriteLine("-----");
+
+
+            
+        }
+    }
+}
