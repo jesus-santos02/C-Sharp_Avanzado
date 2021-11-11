0e2574624d9d9c16b209763ee98d90ee98b64e46
jesusito02 <jesus_restar@hotmail.com>
2021-11-11 16:58:39 -0400

    11NOV2021

---
+++ Exception01/Caldera.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Exception01
+{
+    public class Caldera
+    {
+        private int tempMax = 120;
+        private int tempActual = 0;
+        private string marca = "";
+        private bool funciona = true;
+
+        public int Temperatura { get => tempActual; set => tempActual = value;}
+        public string Marca { get => marca; set => marca = value; }
+        public bool Funciona { get => funciona; }
+
+        public Caldera(string marca, int temp)
+        {
+            this.marca = marca;
+            this.tempActual = temp;
+        }
+
+        //Hacemos trabajar la caldera sin usar excepciones
+        public void Trabajar(int aumento)
+        {
+            if (funciona == false)
+            {
+                System.Console.WriteLine($"La Caldera {marca} esta descompuesta");
+            }
+            else
+            {
+                tempActual += aumento;
+                System.Console.WriteLine($"La temperatura actual es {tempActual}");
+
+                if (tempActual > tempMax)
+                {
+                    System.Console.WriteLine($"{marca} superó la temperatura actual, tiene {tempActual}");
+                    tempActual = tempMax;
+                    funciona = false;
+
+                    //Lanzamos una excepcion y tenemos una variable para la instancia
+                    Exception ex = new Exception(string.Format($"La caldera {marca} se sobrecalienta"));
+                    ex.HelpLink = "http://www.google.com.do";
+
+                    throw ex;
+                }
+            }
+        }
+    }
+}
---
+++ Exception01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exception01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caldera caldera = new Caldera("Mastic-O", 20);
+
+            try
+            {
+                for(int i = 0; i < 10; i++)
+                caldera.Trabajar(20);
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("Sucedió la excepcion");
+                Console.WriteLine($"Este es el metodo: {e.TargetSite}");
+                Console.WriteLine($"Con este mensaje de error: {e.Message}");
+                Console.WriteLine($"Source: {e.Source}");
+                Console.WriteLine($"Clase donde ocurrió: {e.TargetSite.DeclaringType}");
+                Console.WriteLine($"Tipo de miembro: {e.TargetSite.MemberType}");
+                Console.WriteLine($"Stack: {e.StackTrace}");
+            }
+        }
+    }
+}
