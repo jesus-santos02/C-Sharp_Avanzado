2c86c93ca6194aa7bbae005517aff0d6bd953c67
jesusito02 <jesus_restar@hotmail.com>
2021-11-22 15:49:52 -0400

    22NOV2021

---
+++ Colecciones02LinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colecciones02LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //LinkedList es mas eficiente que List cuando se necesita borrar e insertar elementos
+           //a la lista constantemente, ya que esta constituida por nodos
+           LinkedList<int> numeros = new LinkedList<int>();
+           numeros.AddFirst(6);
+
+           foreach (var item in new int[]{8,5,3,5,4,6,7,0,4})
+           {
+               numeros.AddFirst(item);
+           }
+
+           foreach (var numero in numeros)
+           {
+               Console.WriteLine(numero);
+           }   
+
+           Console.WriteLine("-------");  
+
+           for(LinkedListNode<int> nodo = numeros.First; nodo != null; nodo = nodo.Next)
+           {
+               Console.WriteLine(nodo.Value);
+           }       
+
+        }
+    }
+}
